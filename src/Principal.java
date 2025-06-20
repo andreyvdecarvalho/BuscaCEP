21c210355fc807eb85121636dbe9835a74c62db1
Andrey <ay.carvalho@hotmail.com>
2025-06-20 15:26:45 -0300

    Desafio Java - Consumindo API

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Principal {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+
+        System.out.println("Digite o número de CEP para consulta: ");
+        var cep = leitura.nextLine();
+
+        ConsultaCEP consultaCEP = new ConsultaCEP();
+        try{
+            Endereco novoEndereco = consultaCEP.buscaEndereco(cep);
+            System.out.println(novoEndereco);
+            GeradorDeArquivo gerador = new GeradorDeArquivo();
+            gerador.salvaJson(novoEndereco);
+        } catch (RuntimeException | IOException e){
+            System.out.println(e.getMessage());
+            System.out.println("Finalizando a aplicação");
+        }
+
+    }
 }
